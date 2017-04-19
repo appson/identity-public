2b48ee37ae94dfbf921385a84993534cb243f345
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2017-04-19 18:32:23 +0430

    added Email section

--- Client/ClientSections/EmailSection.cs
+++ Client/ClientSections/EmailSection.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Appson.Identity.Client.Model.Email;
 using Appson.Identity.Client.Util;
@@ -7,18 +8,9 @@ namespace Appson.Identity.Client.ClientSections
 {
     public class EmailSection
     {
-        public async Task SendVerificationEmailAsync(string email)
+        public async Task<SendEmailVerificationResponse> SendVerificationEmailAsync(string email)
         {
-            var req = new SendVerificationEmailInput()
-            {
-                Email = email
-            };
-            var response = await HttpHelper.Client.PostAsJsonAsync(EndpointAddresses.EmailSendVerificationEmail, req);
-
-            if (response.IsSuccessStatusCode)
-            {
-                
-            }
+            throw new NotImplementedException();
         }
     }
 }
