0e23dc15849f9e144446a79dbc985a4063b8e586
Kia Panahi Rad <kia.panahirad@gmail.com; kia@appson.ir>
2017-05-20 00:38:40 +0430

    change test method name

--- v0.5/Dotnet SDK/Client.Tests/ConfigurationTests.cs
+++ v0.5/Dotnet SDK/Client.Tests/ConfigurationTests.cs
@@ -8,13 +8,15 @@ namespace Client.Tests
     public class ConfigurationTests
     {
         [TestMethod]
-        public void should_return_configured_client()
+        public void ClientInstancePropertiesAreSetCorrectly()
         {
             var a = new IdentityClient("my-app-id", "https://accounts-staging.appson.ir");
 
             Assert.IsNotNull(a);
-            Assert.IsTrue(a.Configuration.Address.Equals("https://accounts-staging.appson.ir", StringComparison.InvariantCultureIgnoreCase));
-            Assert.IsTrue(a.Configuration.ApplicationId.Equals("my-app-id", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(a.Configuration.Address.Equals("https://accounts-staging.appson.ir",
+                StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(
+                a.Configuration.ApplicationId.Equals("my-app-id", StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
